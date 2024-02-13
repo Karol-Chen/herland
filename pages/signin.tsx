7f603b1fba21f7b3d50aa0cc07b2b0b84484f816
Daixuan Chen <574124970@qq.com>
2024-02-13 17:06:32 -0500

    signup styling

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -5,7 +5,7 @@ import userIcon from "../public/userIcon.svg";
 import passwordIcon from "../public/passwordIcon.svg";
 import logo from "../public/Logo.svg";
 import Image from "next/image";
-import styles from "../styles/api.module.css";
+import styles from "../styles/signin.module.css";
 import Link from "next/link";
 export default function SignIn() {
   return (
@@ -19,17 +19,19 @@ export default function SignIn() {
         </div>
         <div className={styles.signInFrom}>
           <div className={styles.buttonBox}>
-            <button className={styles.btnRed}>Sign In</button>
-            <button className={styles.btnWhite}>Register</button>
+            <Link href="/signin" className={styles.btnRed}>
+              <p>Sign In</p>
+            </Link>
+            <Link href="/signup" className={styles.btnWhite}>
+              <p>Register</p>
+            </Link>
           </div>
           <form className={styles.formBox}>
             <input type="text" placeholder={"username or email"} />
-            <br />
             <input type="password" placeholder="password" />
             <Link href="#" className={styles.fgpwLink}>
               Forget Password?
             </Link>
-            <br />
             <input className={styles.btnLarge} type="submit" value="Sign In" />
           </form>
         </div>
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,30 +1,68 @@
 import Layout from "../components/layout";
 require("dotenv").config();
-
+import styles from "../styles/signup.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import herlandLogo from "../public/cropped-herland.png";
+// import { ReactComponent as BgImage } from "../public/Background.svg";
+import BgImage from "../public/Background.svg";
+import userIcon from "../public/userIcon.svg";
+import passwordIcon from "../public/passwordIcon.svg";
+import logo from "../public/Logo.svg";
 export default function SignUp() {
   function handleSubmit(event) {
     return alert("You have registered successfully");
   }
   return (
-    <Layout>
-      <div className="Registration">
-        <h1>Registration</h1>
-        <form onSubmit={handleSubmit}>
-          username: <input type="text" placeholder="username" />
-          <br />
-          email: <input type="text" placeholder="email" />
-          <br />
-          invitation code: <input type="text" placeholder="invitation code" />
-          <br />
-          password: <input type="password" placeholder="password" />
-          <br />
-          confirm password:{" "}
-          <input type="password" placeholder="confirm password" />
-          <br />
-          <input type="submit" value="Register" />
-          <br />
-        </form>
+    // <Layout>
+    //   <div className="Registration">
+    //     <h1>Registration</h1>
+    //     <form onSubmit={handleSubmit}>
+    //       username: <input type="text" placeholder="username" />
+    //       <br />
+    //       email: <input type="text" placeholder="email" />
+    //       <br />
+    //       invitation code: <input type="text" placeholder="invitation code" />
+    //       <br />
+    //       password: <input type="password" placeholder="password" />
+    //       <br />
+    //       confirm password:{" "}
+    //       <input type="password" placeholder="confirm password" />
+    //       <br />
+    //       <input type="submit" value="Register" />
+    //       <br />
+    //     </form>
+    //   </div>
+    // </Layout>
+    <div className={styles.signInPage}>
+      {/* <img src={BgImage} alt="background image" className={styles.bgImage} />
+       */}
+
+      <div className={styles.signInBox}>
+        <div className={styles.logo}>
+          <Image src={logo} alt="logo"></Image>
+        </div>
+        <div className={styles.signInFrom}>
+          <div className={styles.buttonBox}>
+            <Link href="/signin" className={styles.btnRed}>
+              <p>Sign In</p>
+            </Link>
+            <Link href="/signup" className={styles.btnWhite}>
+              <p>Register</p>
+            </Link>
+          </div>
+          <form className={styles.formBox}>
+            <input type="text" placeholder={"invitation code"} />
+            <input type="text" placeholder={"username or email"} />
+            <input type="password" placeholder="password" />
+            <input type="password" placeholder="confirm password" />
+            <Link href="#" className={styles.fgpwLink}>
+              Invitation Code
+            </Link>
+            <input className={styles.btnLarge} type="submit" value="Register" />
+          </form>
+        </div>
       </div>
-    </Layout>
+    </div>
   );
 }
